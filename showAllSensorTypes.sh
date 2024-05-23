392aead8f312ecfe03461b7a2433f28d3295bb20
Nate Saunders <nathan.saunders@endlas.com>
2024-05-23 09:12:45 -0600

    Added the thermocouples.h file, and scripts for reading the temperatures and showing all the sensor types

---
+++ showAllSensorTypes.sh
@@ -0,0 +1,11 @@
+   echo ''
+   echo 'All Channel Types:'
+   echo "      Channel 1: $(smtc 0 styperd 1)"
+   echo "      Channel 2: $(smtc 0 styperd 2)"
+   echo "      Channel 3: $(smtc 0 styperd 3)"
+   echo "      Channel 4: $(smtc 0 styperd 4)"
+   echo "      Channel 5: $(smtc 0 styperd 5)"
+   echo "      Channel 6: $(smtc 0 styperd 6)"
+   echo "      Channel 7: $(smtc 0 styperd 7)"
+   echo "      Channel 8: $(smtc 0 styperd 8)"
+   echo ''
