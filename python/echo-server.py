190d4e8a399ae76d727ffd691ef0c788eb6ff83f
Nate Saunders <nathan.saunders@endlas.com>
2024-06-16 08:27:11 -0600

    moved the new python server/client scripts into the RPiLaserCube git repository.

---
+++ python/echo-server.py
@@ -0,0 +1,20 @@
+from socket import *
+import os
+
+host = "127.0.0.1"
+
+port = 5432
+
+s = socket(AF_INET, SOCK_STREAM)
+
+s.bind((host,port))
+
+s.listen(5)
+
+print("Listening for connections...")
+
+q,addr = s.accept()
+
+data = input("Enter data to be sent: ")
+encodedData = data.encode();
+q.send(encodedData)  
