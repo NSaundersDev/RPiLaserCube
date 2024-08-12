f17d13958e7f840dd7dbbfce60ad60c0735af38b
Nathan Saunders <NSaundersDev@gmail.com>
2024-08-12 04:19:57 -0600

    Code cleanup and trying to get multiple clients to work proper.

--- html/temperatureServer/log_all_temperatures.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-printf "$(date '+%T'),""$(smtc 0 read 1),""$(smtc 0 read 2),""$(smtc 0 read 3),""$(smtc 0 read 4),""$(smtc 0 read 5),""$(smtc 0 read 6),""$(smtc 0 read 7),""$(smtc 0 read 8)"
-echo "$(date '+%T'),""$(smtc 0 read 1),""$(smtc 0 read 2),""$(smtc 0 read 3),""$(smtc 0 read 4),""$(smtc 0 read 5),""$(smtc 0 read 6),""$(smtc 0 read 7),""$(smtc 0 read 8)" >> temperatures.csv
-sudo cp temperatures.csv /var/www/html/data
--- html/temperatureServer/log_temperatures.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo python log_temperatures.py $1
-sudo cp $1 ../data
